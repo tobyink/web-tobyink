4fa2751d9666d12674bfb4d1f430052af965d65f
Toby Inkster <work@toby.ink>
2021-02-20 00:56:22 +0000

    use ACF for options page

--- parsley/app/options.php
+++ parsley/app/options.php
@@ -1,193 +1,195 @@
 <?php
 
 namespace App;
+use StoutLogic\AcfBuilder\FieldsBuilder;
 
 function theme_colours () {
-  return [ 'primary', 'secondary', 'tertiary', 'quaternary', 'dark', 'light', 'success', 'danger', 'warning', 'info' ];
+	return [ 'primary', 'secondary', 'tertiary', 'quaternary', 'dark', 'light', 'success', 'danger', 'warning', 'info' ];
 }
 
 function theme_get_option ( $id ) {
-  static $json = null;
-  $o = theme_get_options();
-  if ( array_key_exists( $id, $o ) ) {
-    return $o[$id];
-  }
-  if ( ! $json ) {
-    $json = json_decode( file_get_contents( __FILE__ . '.json' ) );
-  }
-  foreach ( $json as $section ) {
-    foreach ( $section->options as $opt ) {
-      if ( $opt->key == $id ) {
-        return $opt->default;
-      }
-    }
-  }
-  return null;
+	$id = str_replace( '-', '_', $id );
+	return get_field( 'parsley_' . $id, 'option' );
 }
 
-function theme_get_options () {
-  static $options = null;
-  if ( ! $options ) {
-    $options = get_option( 'theme_options' );
-  }
-  return $options;
-}
+function theme_write_sass () {
+	$sass = [];
 
-function theme_write_sass ( $options ) {
-  $sass = [];
-  $sass_early = [];
-  $sass_late  = [];
-  foreach ( $options as $k => $v ) {
-    if ( $k == 'font-base' || $k == 'font-headings' ) {
-      $sass_late []= sprintf('$wp-theme-%s: $wp-theme-font-%s;', $k, $v);
-    }
-    elseif ( $k == 'imports' ) {
-      $lines = explode( "\n", $v );
-      foreach ( $lines as $l ) {
-        if ( ! preg_match( '/^http/', $l ) ) {
-          continue;
-        }
-        $sass_early []= sprintf( '@import url(%s);', str_replace( ';', '%3B', trim($l) ) );
-      }
-    }
-    elseif ( ! preg_match( '/^_/', $k ) ) {
-      $sass []= sprintf('$wp-theme-%s: %s;', $k, $v);
-    }
-  }
+	$urls = theme_get_option( 'font-imports' );
+	foreach ( explode( "\n", $urls ) as $l ) {
+		if ( preg_match( '/^http/', $l ) ) {
+			$sass []= sprintf( '@import url(%s);', str_replace( ';', '%3B', trim($l) ) );
+		}
+	}
 
-  $final = "";
-  foreach ( [ $sass_early, $sass, $sass_late ] as $arr ) {
-    foreach ( $arr as $l ) {
-      $final .= $l . "\n";
-    }
-    if ( count($arr) ) {
-      $final .= "\n";
-    }
-  }
+	foreach ( [ 'serif', 'sans', 'monospace', 'display' ] as $f ) {
+		$sass []= sprintf( '$wp-theme-font-%s: %s;', $f, theme_get_option( "font-$f" ) );
+	}
 
-  $file = __DIR__ . '/../resources/assets/styles/common/_wp_theme.scss';
-  file_put_contents( $file, $final );
-  system( "chmod g+rwX '$file'" );
+	foreach ( [ 'base', 'headings' ] as $f ) {
+		$sass []= sprintf('$wp-theme-font-%s: $wp-theme-font-%s;', $f, theme_get_option( "font-$f" ) );
+	}
+
+	foreach ( theme_colours() as $c ) {
+		$sass [] = sprintf( '$wp-theme-colour-%s: %s;', $c, theme_get_option( "colour-$c" ) );
+	}
+
+	foreach ( [ 'page-bg', 'page-text', 'page-link', 'page-muted', 'floating-icons-bg', 'floating-icons-text', 'floating-icons-hover', 'floating-icons-class' ] as $opt ) {
+		$sass [] = sprintf( '$wp-theme-%s: %s;', $opt, theme_get_option( $opt ) );
+	}
+
+	$final = '';
+	foreach ( $sass as $l ) {
+		$final .= "$l\n";
+	}
+
+	$file = __DIR__ . '/../resources/assets/styles/common/_wp_theme.scss';
+	file_put_contents( $file, $final );
+	system( "chmod g+rwX '$file'" );
 }
 
 function theme_compile_sass () {
-  $olddir = getcwd();
-  chdir( __DIR__ . '/..'  );
-  system( './yarn build' );
-  system( 'chmod -R g+rwX ./dist' );
-  chdir( $olddir );
+	$olddir = getcwd();
+	chdir( __DIR__ . '/..'  );
+	system( './yarn build' );
+	system( 'chmod -R g+rwX ./dist' );
+	chdir( $olddir );
 }
 
-if ( is_admin() ) {
-  add_action( 'admin_enqueue_scripts', function () {
-    wp_enqueue_style( 'wp-color-picker' );
-    wp_enqueue_script( 'wp-color-picker' );
-  } );
-  add_action( 'admin_init', function () {
-    register_setting(
-      'theme_options',
-      'theme_options',
-      function ( $options ) {
-        theme_write_sass( $options );
-        theme_compile_sass();
-        return $options;
-      }
-    );
-  } );
-  add_action( 'admin_menu', function () {
-    add_submenu_page(
-      'themes.php',
-      'Theme Options',
-      'Theme Options',
-      'manage_options',
-      'theme-settings',
-      function () {
-        $html = '';
-        $json = json_decode( file_get_contents( __FILE__ . '.json' ) );
-        foreach ( $json as $section ) {
-          $html .= '<h2>' . $section->label . '</h2>';
-          foreach ( $section->options as $opt ) {
-            $val = array_key_exists( 'theme_options', $_POST ) ? $_POST['theme_options'][$opt->key] : theme_get_option( $opt->key );
-            if ( empty($val) ) {
-              $val = $opt->default;
-            }
-            $hval = htmlspecialchars( $val );
-            $html .= '<div style="margin-bottom:1rem">';
-            $html .= '<label for="' . $opt->key . '">' . $opt->label . '</label><br>';
-            if ( $opt->type == 'colour' ) {
-              $html .= '<input type="text" class="colour" name="theme_options[' . $opt->key . ']" id="' . $opt->key . '" value="' . $hval . '" data-default-color="' . $opt->default . '" />';
-            }
-            elseif ( $opt->type == 'colourx') {
-              $html .= '<input type="text" class="colourx" style="width:100%;max-width:30rem" name="theme_options[' . $opt->key . ']" id="' . $opt->key . '" value="' . $hval . '" />';
-              $html .= '<button onclick="document.getElementById(\''.$opt->key.'\').value=\''.htmlspecialchars(addslashes($opt->default)).'\';return false">Default</button>';
-            }
-            elseif ( $opt->type == 'select' ) {
-              $html .= '<select name="theme_options[' . $opt->key . ']" id="' . $opt->key . '">';
-              foreach ( $opt->choices as $choice ) {
-                $html .= sprintf(
-                  '<option %s value="%s">%s%s</option>',
-                  ( ( $choice == $val ) ? 'selected' : '' ),
-                  htmlspecialchars($choice),
-                  htmlspecialchars($choice),
-                  ( ( $choice == $opt->default ) ? ' [default]' : '' ),
-                );
-              }
-              $html .= '</select>';
-            }
-            elseif ( $opt->type == 'textarea' ) {
-              $html .= '<textarea cols="40" rows="6" style="width:100%;max-width:40rem" name="theme_options[' . $opt->key . ']" id="' . $opt->key . '">' . $hval . '</textarea>';
-            }
-            else {
-              $html .= '<input type="text" style="width:100%;max-width:40rem" name="theme_options[' . $opt->key . ']" id="' . $opt->key . '" value="' . $hval . '" />';
-              $html .= '<button onclick="document.getElementById(\''.$opt->key.'\').value=\''.htmlspecialchars(addslashes($opt->default)).'\';return false">Default</button>';
-            }
-            $html .= '</div>';
-          }
-        }
-        $html .= '<script>
-          var palette_map = {};
-          var palette     = [];
-          jQuery( document ).ready( function ($) {
-            $(".colour").wpColorPicker();
+add_action( 'acf/save_post', function () {
+	$screen = get_current_screen();
+	if ( strpos( $screen->id, "parsley-options" ) == true ) {
+		theme_write_sass();
+		theme_compile_sass();
+	}
+}, 99 );
 
-            $(".colourx").iris( {
-              mode: "hsv",
-              controls: { horiz: "h", vert: "v", strip: "s" },
-              change: function ( e, ui ) {
-                var $this = $( e.target );
-                $this.iris( "hide" );
-                var c = ui.color.toString();
-                setTimeout( function () {
-                  if ( palette_map[c] ) {
-                    $this.val( "$wp-theme-" + palette_map[c] );
-                  }
-                }, 300 );
-              },
-            } ).click( function () {
-              var $this = $(this);
-              palette_map = {};
-              palette     = [];
-              $(".colour").each( function ( ix, e ) {
-                var $e = jQuery(e);
-                palette_map[ $e.val() ] = $e.attr( "id" );
-                palette.push( $e.val() );
-              } );
-              $this.iris( "option", "palettes", palette );
-              $this.iris( "show" );
-            } );
-          } );
-        </script>';
+add_action( 'acf/init', function () {
 
-        echo '<div class="wrap">';
-        echo '<h1>Theme Options</h1>';
-        echo '<form method="post" action="options.php">';
-        echo settings_fields('theme_options');
-        echo $html;
-        echo submit_button();
-        echo "<p>Saving changes may take a minute; the site's stylesheets will be recompiled after each change.</p>";
-        echo '</form>';
-        echo '</div>';
-      }
-    );
-  } );
-}
+	acf_add_options_page( [
+		'page_title' => 'Parsley Options',
+		'menu_title' => 'Parsley Options',
+		'menu_slug'  => 'parsley-options',
+		'capability' => 'edit_theme_options',
+		'position'   => '61',
+		'autoload'   => true,
+	] );
+
+	$opts = new FieldsBuilder( 'colours' );
+	$opts->setLocation( 'options_page', '==', 'parsley-options' );
+
+	$colour_defaults = [
+		'primary'     => '#007bff',
+		'secondary'   => '#6c757d',
+		'tertiary'    => '#fd7e14',
+		'quaternary'  => '#20c997',
+		'dark'        => '#343a40',
+		'light'       => '#f8f9fa',
+		'success'     => '#28a745',
+		'danger'      => '#dc3545',
+		'warning'     => '#ffc107',
+		'info'        => '#17a2b8',
+	];
+
+	foreach ( $colour_defaults as $label => $default ) {
+		$opts->addColorPicker( "parsley_colour_$label", [
+			'label'          => ucfirst($label),
+			'default_value'  => $default,
+			'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+		] );
+	}
+
+	$opts->addText( 'parsley_page_bg', [
+		'label'          => 'Body Background Colour',
+		'default_value'  => 'white',
+		'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+	] );
+
+	$opts->addText( 'parsley_page_text', [
+		'label'          => 'Text Colour',
+		'default_value'  => '$wp-theme-colour-dark',
+		'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+	] );
+
+	$opts->addText( 'parsley_page_link', [
+		'label'          => 'Link Colour',
+		'default_value'  => 'lighten($wp-theme-colour-primary, 10%)',
+		'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+	] );
+
+	$opts->addText( 'parsley_page_muted', [
+		'label'          => 'Muted Text Colour',
+		'default_value'  => 'lighten($wp-theme-colour-dark, 30%)',
+		'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+	] );
+
+	acf_add_local_field_group( $opts->build() );
+
+	$opts = new FieldsBuilder( 'fonts' );
+	$opts->setLocation( 'options_page', '==', 'parsley-options' );
+
+	$font_defaults = [
+		'serif'     => "Georgia, 'Times New Roman', Times, serif",
+		'sans'      => "'Helvetica Neue', Helvetica, Arial, sans-serif",
+		'monospace' => "Menlo, Monaco, Consolas, 'Courier New', monospace",
+		'display'   => "Techno, Impact, sans-serif",
+	];
+
+	foreach ( $font_defaults as $label => $default ) {
+		$opts->addText( "parsley_font_$label", [
+			'label'          => ucfirst($label),
+			'default_value'  => $default,
+			'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+		] );
+	}
+
+	$opts->addSelect( 'parsley_font_base', [
+		'label'          => 'Base font',
+		'default_value'  => 'serif',
+		'choices'        => array_keys( $font_defaults ),
+		'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+	] );
+
+	$opts->addSelect( 'parsley_font_headings', [
+		'label'          => 'Headings font',
+		'default_value'  => 'display',
+		'choices'        => array_keys( $font_defaults ),
+		'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+	] );
+
+	$opts->addTextarea( 'parsley_font_imports', [
+		'label'          => 'Import URLs',
+	] );
+
+	acf_add_local_field_group( $opts->build() );
+
+	$opts = new FieldsBuilder( 'floating_menu' );
+	$opts->setLocation( 'options_page', '==', 'parsley-options' );
+
+	$opts->addText( 'parsley_floating_icons_bg', [
+		'label'          => 'Background Colour',
+		'default_value'  => '$wp-theme-colour-primary',
+		'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+	] );
+
+	$opts->addText( 'parsley_floating_icons_text', [
+		'label'          => 'Foreground Colour',
+		'default_value'  => 'white',
+		'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+	] );
+
+	$opts->addText( 'parsley_floating_icons_hover', [
+		'label'          => 'Hover Background Colour',
+		'default_value'  => 'lighten($wp-theme-colour-primary, 10%)',
+		'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+	] );
+
+	$opts->addSelect( 'parsley_floating_icons_class', [
+		'label'          => 'Class',
+		'default_value'  => 'floating-icons-left',
+		'choices'        => [ 'floating-icons-left', 'floating-icons-right' ],
+		'wrapper'        => [ 'width' => '50%', 'id' => '', 'class' => '' ],
+	] );
+
+	acf_add_local_field_group( $opts->build() );
+} );
